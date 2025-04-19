6e45e069bd400e4ec975a748a0deaaffc46eee58
Genius Graphics & Infotech <geniusgraphicsdev07@gmail.com>
2025-04-19 18:35:39 +0530

    commit

--- src/security/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem("authToken");
-  return token ? children : <Navigate to="/auth/sign-in" replace />;
-};
-
-export default PrivateRoute;
--- src/utils/api.js
+++
@@ -1,2 +0,0 @@
-export const Base_url = "http://localhost:5001/api";
-// export const Base_url = "https://vivavista-backend.onrender.com/api";
